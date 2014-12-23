583f36ebfdf58ac35a9e02daa7671234bbf6bbbb
Javier Murillo <jhmmurillo@gmail.com>
2014-12-23 08:14:09 -0800

    add example to workspace

---
+++ ConektaExample/ConektaExample/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ConektaExample
+//
+//  Created by Javier Murillo on 12/22/14.
+//  Copyright (c) 2014 Conekta.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!;
+    @IBOutlet var numberField: UITextField!;
+    @IBOutlet var expMonthField: UITextField!;
+    @IBOutlet var expYearField: UITextField!;
+    @IBOutlet var ccField: UITextField!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func tokenizeCard(sender : AnyObject) {
+        var conekta = Conekta(publicKey: "key_EVryd61Uhsq9d6Z2")
+        
+        var tarjeta = Card(last4: numberField.text, name: nameField.text, cvc: ccField.text, exp_month: expMonthField.text, exp_year: expYearField.text)
+
+        conekta.createToken(tarjeta, withSuccess: { () -> Void in
+            print("tokenizada")
+            }, withFailure: { () -> Void in
+            print("fail")
+        })
+        
+    }
+    
+}
