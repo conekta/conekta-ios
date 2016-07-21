a9d75fd5c4b186c593ce0719638d19e03b5e8a0e
Ricardo Michel Reyes <me@richiereyes.com>
2016-07-21 16:35:21 -0700

    Port completely to Swift

---
+++ Conekta/ConektaSwift/ConektaSwift/ConektaSwift.m
@@ -0,0 +1,13 @@
+//
+//  ConektaSwift.m
+//  ConektaSwift
+//
+//  Created by Ricardo Michel Reyes Martínez on 7/21/16.
+//  Copyright © 2016 Ricardo Michel Reyes Martínez. All rights reserved.
+//
+
+#import "ConektaSwift.h"
+
+@implementation ConektaSwift
+
+@end
