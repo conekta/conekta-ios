337bdd13802c87830ad2a37ea55d8730bf7fb845
Eduardo Pacheco <eduardo_pacheco@me.com>
2017-06-28 15:15:18 -0500

    Project updated to Swift 3 and elimination of innecesary files.

--- Examples/ConektaCardToken-Swift/ConektaCardToken-SwiftTests/ConektaCardToken_SwiftTests.swift
+++ Examples/ConektaCardToken-Swift/ConektaCardToken-SwiftTests/ConektaCardToken_SwiftTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import ConektaCardToken_Swift
 
 class ConektaCardToken_SwiftTests: XCTestCase {
     
@@ -28,7 +27,7 @@ class ConektaCardToken_SwiftTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
