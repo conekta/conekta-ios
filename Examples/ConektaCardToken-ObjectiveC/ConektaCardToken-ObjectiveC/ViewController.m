7f565f3bfa8f7e4f9515e0ea7f9434c383a8dac3
erickCTA <46541591+erickCTA@users.noreply.github.com>
2019-09-03 16:51:07 -0500

    Hotfix/remove unused logs (#18)
    * hotfix / remove unused logs
    
    Why is this change neccesary?
    
    To not show significant data in  the logs
    
    How does it address the issue?
    
    removing unused logs
    
    What side effects does this change have?
    
    Can affect data clients
    
    * updated swift version to 4
    
    * Update and Remove token print´s
    
    * added comments in log in view controller for objective c
    
    * changed expire date harcoded to 2020

--- Examples/ConektaCardToken-ObjectiveC/ConektaCardToken-ObjectiveC/ViewController.m
+++ Examples/ConektaCardToken-ObjectiveC/ConektaCardToken-ObjectiveC/ViewController.m
@@ -30,14 +30,14 @@
     
     /* Tokenize card */
     
-    Card *card = [conekta.Card initWithNumber: @"4242424242424242" name: @"Julian Ceballos" cvc: @"123" expMonth: @"10" expYear: @"2018"];
+    Card *card = [conekta.Card initWithNumber: @"4242424242424242" name: @"Julian Ceballos" cvc: @"123" expMonth: @"10" expYear: @"2020"];
     
     Token *token = [conekta.Token initWithCard:card];
     
     [token createWithSuccess: ^(NSDictionary *data) {
-        NSLog(@"%@", data);
+       // NSLog(@"%@", data);
     } andError: ^(NSError *error) {
-        NSLog(@"%@", error);
+       // NSLog(@"%@", error);
     }];
 }
 
--- Examples/ConektaCardToken-Swift/ConektaCardToken-Swift/ViewController.swift
+++ Examples/ConektaCardToken-Swift/ConektaCardToken-Swift/ViewController.swift
@@ -21,18 +21,18 @@ class ViewController: UIViewController {
         
         conekta.collectDevice()
         
-        let card = conekta.Card()
+        let card = conekta.card()
         
-        card.setNumber("4242424242424242", name: "Julian Ceballos", cvc: "123", expMonth: "10", expYear: "2018")
+        card?.setNumber("4242424242424242", name: "Julian Ceballos", cvc: "123", expMonth: "10", expYear: "2020")
         
-        let token = conekta.Token()
+        let token = conekta.token()
         
-        token.card = card
+        token?.card = card
         
-        token.createWithSuccess({ (data) -> Void in
-            print(data)
+        token?.create(success: { (data) -> Void in
+            //print(data)
             }, andError: { (error) -> Void in
-                print(error)
+                //print(error)
         })
     }
 
@@ -43,4 +43,3 @@ class ViewController: UIViewController {
 
 
 }
-
