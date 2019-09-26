a18dd362b639d60e2e2cdc3a3f841c99a8e273b2
NyabeConekta <46569182+NyabeConekta@users.noreply.github.com>
2019-09-26 16:51:46 -0500

    Create and Update Layouts (#19)

--- Examples/ConektaCardToken-Swift/ConektaCardToken-Swift/ViewController.swift
+++ Examples/ConektaCardToken-Swift/ConektaCardToken-Swift/ViewController.swift
@@ -10,36 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var numberCardUI: UITextField!
+    @IBOutlet weak var nameCardUI: UITextField!
+    @IBOutlet weak var expMonthUI: UITextField!
+    @IBOutlet weak var expYearUI: UITextField!
+    @IBOutlet weak var cvcUI: UITextField!
+    @IBOutlet weak var outTokenUI: UILabel!
+    @IBOutlet weak var outUUID_UI: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let conekta = Conekta()
-        
-        conekta.delegate = self
-        
-        conekta.publicKey = "key_KJysdbf6PotS2ut2"
-        
-        conekta.collectDevice()
-        
-        let card = conekta.card()
-        
-        card?.setNumber("4242424242424242", name: "Julian Ceballos", cvc: "123", expMonth: "10", expYear: "2020")
-        
-        let token = conekta.token()
-        
-        token?.card = card
-        
-        token?.create(success: { (data) -> Void in
-            //print(data)
-            }, andError: { (error) -> Void in
-                //print(error)
-        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func tokenizeBtn(_ sender: UIButton) {
+            
+        let conekta = Conekta()
+        conekta.delegate = self
+        conekta.publicKey = "key_KJysdbf6PotS2ut2"
+        conekta.collectDevice()
+        
+        let card = conekta.card()
+        
+        card?.setNumber(numberCardUI.text, name: nameCardUI.text, cvc: cvcUI.text , expMonth: expMonthUI.text, expYear: expYearUI.text)
+        
+        let token = conekta.token()
+        token?.card = card
+        
+        token?.create(success: { (data) -> Void in
+            if let data = data as NSDictionary? as! [String:Any]? {
+                self.outTokenUI.text = data["id"] as? String
+                self.outUUID_UI.text = conekta.deviceFingerprint()
+            }
+        }, andError: { (error) -> Void in
+            //print(error)
+        })
+    }
 }
